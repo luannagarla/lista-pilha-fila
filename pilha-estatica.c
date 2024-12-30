f09e2c748108698529d03bae6da6b1e571c72966
Luanna Garla <luagarla@gmail.com>
2024-12-30 13:18:06 -0300

    exibirPilhaInversa implementado

--- pilha-estatica.c
+++ pilha-estatica.c
@@ -1,4 +1,4 @@
-//pilha estatica print inverso
+// pilha estatica com print inverso 
 
 #include <stdio.h>
 #include <stdbool.h>
@@ -6,58 +6,82 @@
 #define MAX 5
 typedef int TIPOCHAVE;
 
-typedef struct {
+typedef struct
+{
     TIPOCHAVE chave;
 } REGISTRO;
 
-typedef struct {
+typedef struct
+{
     int topo;
     REGISTRO A[MAX];
 } PILHA;
 
-void inicializarPilha(PILHA* p) {
+void inicializarPilha(PILHA *p)
+{
     p->topo = -1;
 }
 
-bool inserirElementoPilha(PILHA* p, REGISTRO reg) {
-    if (p->topo < MAX - 1) { 
-        p->topo++; //incrementa topo
-        p->A[p->topo] = reg; //add
+bool inserirElementoPilha(PILHA *p, REGISTRO reg)
+{
+    if (p->topo < MAX - 1)
+    {
+        p->topo++;           // incrementa topo
+        p->A[p->topo] = reg; // add
         return true;
     }
     return false; // pilha cheia
 }
 
 // Código retirado do class e adaptado
-void inserir(PILHA* p, TIPOCHAVE ch) {
-    
+void inserir(PILHA *p, TIPOCHAVE ch)
+{
+
     REGISTRO reg;
     reg.chave = ch;
     if (!inserirElementoPilha(p, reg))
         printf("Nao foi possivel inserir elemento %i.\n", ch);
 }
 
-//print comum
-void exibirPilha(PILHA* p) {
+// print comum
+void exibirPilha(PILHA *p)
+{
     printf("\nPilha do topo ate a base: \n");
     int i;
-    for (i = p->topo; i >= 0; i--) {
+
+    for (i = p->topo; i >= 0; i--) // começa do topo e vai até 0
+    { 
         printf("%i ", p->A[i].chave);
     }
     printf("\"\n");
 }
 
-int main() {
+void exibirPilhaInvertida(PILHA *p)
+{
+    printf("\nPilha da base ate o topo: \n");
+    int i;
+
+    for (i = 0; i <= p->topo; i++) // começa em 0 e vai até o topo
+    { 
+        printf("%i ", p->A[i].chave);
+    }
+    printf("\n");
+}
+
+int main()
+{
     PILHA p;
-    inicializarPilha(&p); 
+    inicializarPilha(&p);
 
     inserir(&p, 10);
     inserir(&p, 20);
     inserir(&p, 30);
     inserir(&p, 40);
     inserir(&p, 50);
-    inserir(&p, 60); //teste de pilha cheia
+    inserir(&p, 60); // teste de pilha cheia                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+
     exibirPilha(&p);
+    exibirPilhaInvertida(&p);
 
     return 0;
 }
